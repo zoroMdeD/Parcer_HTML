3c6a03d51abbde31bab6d3c9ae388dfe9a897153
Морозов Максим <mmorozo@ask.ru>
2021-12-29 13:22:15 +0300

    Add read PN and SerialNum

---
+++ Parcer_HTML/Parcer_HTML/ExcelTest.cs
@@ -0,0 +1,73 @@
+﻿using LinqToExcel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcer_HTML
+{
+    public class ConnexionExcel
+    {
+        public string _pathExcelFile;
+        public ExcelQueryFactory _urlConnexion;
+        public ConnexionExcel(string path)
+        {
+            this._pathExcelFile = path;
+            this._urlConnexion = new ExcelQueryFactory(_pathExcelFile);
+        }
+        public string PathExcelFile
+        {
+            get
+            {
+                return _pathExcelFile;
+            }
+        }
+        public ExcelQueryFactory UrlConnexion
+        {
+            get
+            {
+                return _urlConnexion;
+            }
+        }
+    }
+
+    public class Product
+    {
+        public int ProductId
+        {
+            get;
+            set;
+        }
+        public string PartNumber
+        {
+            get;
+            set;
+        }
+        public string CategoryName
+        {
+            get;
+            set;
+        }
+
+        public void FuncOne()
+        {
+            List<string> MassPartNumber = new List<string>();
+            string pathToExcelFile = @"D:\TestExcel.xlsx";
+            ConnexionExcel ConxObject = new ConnexionExcel(pathToExcelFile);
+            //Query a worksheet with a header row (sintax SQL-Like LINQ)
+            var query1 = from a in ConxObject.UrlConnexion.Worksheet<Product>("Лист1")
+                         select a;
+            foreach (var result in query1)
+            {
+                string products = "{0}";
+                //Console.WriteLine(string.Format(products, result.PartNumber));
+                MassPartNumber.Add(result.PartNumber);
+            }
+            for(int i = 0; i < MassPartNumber.Count; i++)
+                Console.WriteLine(MassPartNumber[i]);
+            
+            Console.ReadKey();
+        }
+    }
+}
--- Parcer_HTML/Parcer_HTML/Program.cs
+++ Parcer_HTML/Parcer_HTML/Program.cs
@@ -7,6 +7,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System.Threading;
 using OpenQA.Selenium.Support.UI;
+using LinqToExcel;
 
 namespace Parcer_HTML
 {
@@ -14,36 +15,48 @@ namespace Parcer_HTML
     {
         static void Main(string[] args)
         {
-            string str = "STM32F103C8T6";
+            string str = "Кер. ЧИП Конд. 0.01 мкф X7R 50В 10% 0603, Конденсатор керамический SMD";
 
+            ChromeDriverService chromeservice = ChromeDriverService.CreateDefaultService();            
             ChromeOptions options = new ChromeOptions();
-            options.AddArguments("--headless");     //Делаем окно браузера невидимым
 
-            IWebDriver driver = new ChromeDriver(options);     //Передаем флаг в браузер
-            driver.Url = @"http://digikey.com";
+            chromeservice.HideCommandPromptWindow = true;    //Спрятать консоль
+            //options.AddArguments("--headless");     //Делаем окно браузера невидимым
+            IWebDriver driver = new ChromeDriver(chromeservice, options);     //Передаем флаг в браузер
+
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3));    //Инициализация задержки в 3 секунды
+
+            driver.Url = @"https://www.chipdip.ru/";
+        
+            driver.FindElement(By.XPath(@".//input[@name='searchtext']")).SendKeys(str);  //AD5516ABCZ-1     STM32F103C8T6
+            driver.FindElement(By.XPath(@".//button[@class='btn-reset header__button header__search-button']")).Click();
             Delay();
-
-            driver.FindElement(By.XPath(@".//div[@class='searchbox-inner-searchtext']/input[@name='keywords']")).SendKeys(str);  //AD5516ABCZ-1     STM32F103C8T6
-            driver.FindElement(By.XPath(@".//div[@class='searchbox-inner-searchbutton']/button[@class='search-button']")).Click();
-            Delay();
-
+        
             try
             {
-                driver.FindElement(By.XPath($@".//img[@title='{str}']")).Click();
+                //driver.FindElement(By.XPath($@".//main/tbody/tr[2]/td/div/a[@class='link']")).Click();  //img[@title='{str}']
+                driver.FindElement(By.XPath(@".//a[@class='link']/b")).Click();     //Очень не точно!!! нужно оптимизировать
                 Delay();
             }
             catch (Exception)
             {
                 Console.WriteLine("miss");
             }
-
-            var links = driver.FindElements(By.XPath(@".//table[@id='product-attributes']/tbody/tr[1]"));   //@".//table[@id='product-attributes']/tbody/tr[@class='MuiTableRow-root']"        @".//li[@class='MuiBreadcrumbs-li']/a"
-            foreach (IWebElement link in links)
+        
+            var links1 = driver.FindElements(By.XPath(@".//div[@class='product_main-ids ptext']/div[@class='product_main-id'][1]/span[2]"));   //@".//table[@id='product-attributes']/tbody/tr[@class='MuiTableRow-root']"        @".//li[@class='MuiBreadcrumbs-li']/a"
+            foreach (IWebElement link in links1)
             {
                 Console.WriteLine(link.Text);
             }
+            var links2 = driver.FindElements(By.XPath(@".//div[@class='product_main-ids ptext']/div[@class='product_main-id'][3]/span[@itemprop='mpn']"));   //@".//table[@id='product-attributes']/tbody/tr[@class='MuiTableRow-root']"        @".//li[@class='MuiBreadcrumbs-li']/a"
+            foreach (IWebElement link in links2)
+            {
+                Console.WriteLine(link.Text);
+            }
+            Console.ReadLine();
         }
-        static void Delay(int mils = 1000)
+        static void Delay(int mils = 500)
         {
             Thread.Sleep(mils);
         }
